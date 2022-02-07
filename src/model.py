5703c31f3accb99b2e5ddfb315b699d3633f8e1f
preke <geek00021@gmail.com>
2022-02-07 22:56:49 +0800

    .

--- src/model.py
+++ src/model.py
@@ -30,19 +30,18 @@ class Emo_Generation(BertPreTrainedModel):
         self.num_labels = 7
         self.mid_size = 100 
         self.bert = BertModel(config)
-        self.uttr_to_vad = nn.Linear(config.hidden_size, 3)
         
+        self.mood_to_hidden = nn.Linear(3, config.hidden_size)
         # self.classifier = ClassificationHead(3, config.hidden_size, 7)
         # self.emo_recognizer = nn.Linear(config.hidden_size, 3)
-        self.classifier = nn.Linear(3, 7)
+        self.classifier = nn.Linear(config.hidden_size, 7)
 
     def forward(self, input_ids, attn_masks, uttr_vad, personality, init_mood):
         
         bert_outputs = self.bert(input_ids, attention_mask=attn_masks)
         bert_hidden = bert_outputs[1]
         response_mood = init_mood + uttr_vad * personality
-        
-        emo_embedding = self.uttr_to_vad(bert_hidden) * personality + response_mood
+        emo_embedding = bert_hidden + self.mood_to_hidden(response_mood)
         # user_emo = self.emo_recognizer(bert_hidden)
         response_emo = self.classifier(emo_embedding)
         return response_emo, response_mood #, user_emo
