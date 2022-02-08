c3cd88be738e9def2f0271f00e522cfdf139da2c
Zhiyuan <geek00021@gmail.com>
2022-02-08 14:11:46 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -18,38 +18,41 @@ args = parser.parse_args()
 
 args.device        = 1
 args.base          = 'BERT'
-args.SEED          = 42
+
 args.MAX_LEN       = 256 
 args.batch_size    = 16
 args.lr            = 1e-5
 args.adam_epsilon  = 1e-8
 args.epochs        = 50
-args.result_name   = args.base + '_Personality_back_no_dropout_epochs_'+str(args.epochs)+'_Batchsize_'+str(args.batch_size)+'_.csv'
 
 
+seed_list = [0,1,2,41,42,123, 234, 345, 456, 567]
+for seed in  seed_list:
+    args.SEED          = seed
+    args.result_name   = str(args.SEED) + '.csv'
 
-np.random.seed(args.SEED)
-torch.manual_seed(args.SEED)
-torch.cuda.manual_seed_all(args.SEED)
-
-
-## LOAD DATA
-from dataload import load_data
-train_length, train_dataloader, valid_dataloader, test_dataloader = load_data(args, DATA_PATH)
-args.train_length = train_length
-
-## TRAIN THE MODEL
-from model import Emo_Generation
-from train import train_model
+    np.random.seed(args.SEED)
+    torch.manual_seed(args.SEED)
+    torch.cuda.manual_seed_all(args.SEED)
 
 
-if args.base == 'RoBERTa':
-    model = Emo_Generation.from_pretrained('roberta-base').cuda(args.device)
-else:
-    model = Emo_Generation.from_pretrained('bert-base-uncased').cuda(args.device)
-    
-    
-train_model(model, args, train_dataloader, valid_dataloader, test_dataloader)
+    ## LOAD DATA
+    from dataload import load_data
+    train_length, train_dataloader, valid_dataloader, test_dataloader = load_data(args, DATA_PATH)
+    args.train_length = train_length
+
+    ## TRAIN THE MODEL
+    from model import Emo_Generation
+    from train import train_model
+
+
+    if args.base == 'RoBERTa':
+        model = Emo_Generation.from_pretrained('roberta-base').cuda(args.device)
+    else:
+        model = Emo_Generation.from_pretrained('bert-base-uncased').cuda(args.device)
+        
+        
+    train_model(model, args, train_dataloader, valid_dataloader, test_dataloader)
 
 
 
