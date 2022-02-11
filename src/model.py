0ad40fe6884fc5749678feca846b3e765aa69f80
preke <geek00021@gmail.com>
2022-02-11 14:46:40 +0800

    .

--- src/model.py
+++ src/model.py
@@ -31,8 +31,8 @@ class Emo_Generation(BertPreTrainedModel):
         self.mid_size = 100
         self.bert = BertModel(config)
         self.mood_dense = Dense(3, config.hidden_size, 3)
-        self.mood_to_hidden = Dense(3, config.hidden_size, config.hidden_size/2)
-        self.hidden_resize = Dense(config.hidden_size, config.hidden_size, config.hidden_size/2)
+        self.mood_to_hidden = Dense(3, config.hidden_size, config.hidden_size * 0.5)
+        self.hidden_resize = Dense(config.hidden_size, config.hidden_size, config.hidden_size * 0.5)
         self.personality_to_hidden = nn.Linear(3, config.hidden_size)
         self.classifier = nn.Linear(config.hidden_size, 7)
 
