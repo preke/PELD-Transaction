8c7a7216f3e40ead9f72b2e0bc6019ae246c3462
preke <geek00021@gmail.com>
2022-02-11 15:05:51 +0800

    .

--- src/train.py
+++ src/train.py
@@ -19,14 +19,14 @@ def train_model(model, args, train_dataloader, valid_dataloader, test_dataloader
     num_training_steps = len(train_dataloader)*args.epochs
     
     
-    for name, param in model.named_parameters(): 
-        if name.startswith('bert'):
-            param.requires_grad = False
-        else:
-            print(name,param.size())
-        if name.startswith('bert.pooler') or name.startswith('bert.encoder.layer.10') or name.startswith('bert.encoder.layer.11'):
-            param.requires_grad = True
-            print(name, param.size())
+    # for name, param in model.named_parameters(): 
+    #     if name.startswith('bert'):
+    #         param.requires_grad = False
+    #     else:
+    #         print(name,param.size())
+    #     if name.startswith('bert.pooler') or name.startswith('bert.encoder.layer.10') or name.startswith('bert.encoder.layer.11'):
+    #         param.requires_grad = True
+    #         print(name, param.size())
   
     
             
