0dacacf124e330517d377ae8ccf2af1511678b80
preke <geek00021@gmail.com>
2022-02-24 12:16:19 +0800

    .

--- src/model.py
+++ src/model.py
@@ -31,10 +31,11 @@ class Emo_Generation(BertPreTrainedModel):
         self.num_labels = 7
         self.mid_size = int(config.hidden_size * 0.5)
         self.bert = BertModel(config)
-        self.mood_dense = Dense(self.mid_size + 1, config.hidden_size, 4)
+        self.mood_dense = Dense(4, config.hidden_size, 4)
+        self.mood_expand = Dense(1, config.hidden_size, 4)
         self.mood_to_hidden = Dense(4, config.hidden_size, self.mid_size)
 
-        self.hidden_resize_1 = Dense(config.hidden_size, config.hidden_size, self.mid_size)
+        self.hidden_resize_1 = Dense(config.hidden_size, config.hidden_size, 4)
         self.hidden_resize_2 = Dense(config.hidden_size, config.hidden_size, self.mid_size)
 
         self.personality_to_hidden = nn.Linear(3, config.hidden_size)
@@ -48,7 +49,7 @@ class Emo_Generation(BertPreTrainedModel):
         bert_outputs   = self.bert(input_ids, attention_mask=attn_masks)
         bert_hidden    = bert_outputs[1]
 
-        mood_v = torch.cat((init_mood.view(-1,1).float(), self.hidden_resize_1(bert_hidden)), 1)
+        mood_v = self.mood_expand(init_mood.view(-1,1).float()) +  self.hidden_resize_1(bert_hidden)
         response_mood  = self.mood_dense(mood_v)
 
         # emo_embedding  = torch.cat((self.mood_to_hidden(response_mood), self.hidden_resize(bert_hidden)), 1) + self.personality_to_hidden(personality)
--- src/train.py
+++ src/train.py
@@ -78,7 +78,7 @@ def train_model(model, args, train_dataloader, valid_dataloader, test_dataloader
             emo_loss      = emo_loss_fct(logits, b_labels)
             mood_loss     = mood_loss_fct(m_r, b_response_mood)
             # user_loss     = user_loss_fct(user_emo, b_user_emo)
-            loss          = emo_loss + mood_loss*1.0 # + user_loss
+            loss          = mood_loss # emo_loss + mood_loss*1.0 # + user_loss
                         
 
             
