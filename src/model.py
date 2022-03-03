e3c71397883391b76e5d00d3f5b0f3dc81ecfd5d
preke <geek00021@gmail.com>
2022-03-04 00:24:23 +0800

    .

--- src/model.py
+++ src/model.py
@@ -55,7 +55,7 @@ class Emo_Generation(BertPreTrainedModel):
         # bert_hidden: batch_size * 32
         delta_mood = torch.cat((uttr_vad, self.hidden_resize(bert_hidden)), 1) 
 
-        response_mood  = F.softmax(self.mood_dense(delta_mood)) + init_mood*5 # scaling by 5
+        response_mood  = F.softmax(self.mood_dense(delta_mood)) + init_mood#*5 # scaling by 5
         emo_embedding  = torch.cat((self.mood_to_hidden(response_mood), self.hidden_resize_2(bert_hidden),  self.personality_to_hidden(personality)), 1)
         response_emo   = self.classifier(emo_embedding)
         return response_emo, response_mood 
