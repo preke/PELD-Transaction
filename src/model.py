8a43a10b523b94c7dd9b02d6a85f130af67ca72c
preke <geek00021@gmail.com>
2022-03-04 02:26:59 +0800

    .

--- src/model.py
+++ src/model.py
@@ -30,7 +30,7 @@ class Emo_Generation(BertPreTrainedModel):
         super().__init__(config)
         self.num_labels = 7
         self.bert = BertModel(config)
-        self.mid_size = 256
+        self.mid_size = 768
 
 
         self.mood_dense = Dense(self.mid_size+3, config.hidden_size, 3)
