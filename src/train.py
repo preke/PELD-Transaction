b3dbcc83bda1a14e55f7b7c866d171ac1196080d
preke <geek00021@gmail.com>
2022-03-10 15:10:49 +0800

    .

--- src/train.py
+++ src/train.py
@@ -80,18 +80,8 @@ def train_model(model, args, train_dataloader, valid_dataloader, test_dataloader
             emo_loss      = emo_loss_fct(logits, b_labels)
             mood_loss     = mood_loss_fct(m_r, b_response_mood)
 
-            
-
             # user_loss     = user_loss_fct(user_emo, b_user_emo)
             loss          = emo_loss + mood_loss*1.0 # + user_loss
-                 
-            
-
-            print(m_r)
-
-            print(b_labels)
-
-            print(b_response_mood)
             
             logits        = logits.detach().to('cpu').numpy()
             label_ids     = b_labels.to('cpu').numpy()                
