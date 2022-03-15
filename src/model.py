e012a8b787bb2ba98d7a1e38377c804f61eeafb1
preke <geek00021@gmail.com>
2022-03-15 15:48:33 +0800

    .

--- src/model.py
+++ src/model.py
@@ -36,6 +36,9 @@ class Emo_Generation(BertPreTrainedModel):
         self.mood_to_hidden = Dense(3, config.hidden_size, self.mid_size)
         self.mood_to_logit = Dense(3, config.hidden_size, 4)
 
+        self.hidden_resize = Dense(config.hidden_size, config.hidden_size, self.mid_size)
+        self.hidden_resize_2 = Dense(config.hidden_size, config.hidden_size, self.mid_size)
+
         self.personality_to_hidden = nn.Linear(3, self.mid_size)
         self.personality_dense = Dense(3, self.mid_size, 3)
 
@@ -48,13 +51,13 @@ class Emo_Generation(BertPreTrainedModel):
         bert_outputs   = self.bert(input_ids, attention_mask=attn_masks)
         bert_hidden    = bert_outputs[1]
 
-        delta_mood = torch.cat((uttr_vad, bert_hidden), 1) 
+        delta_mood = torch.cat((uttr_vad, self.hidden_resize(bert_hidden)), 1) 
 
         response_mood_vad    = F.softmax(self.mood_dense(delta_mood) * self.personality_dense(personality)) * 2 + init_mood
         response_mood_logits = self.mood_to_logit(response_mood_vad)
 
 
-        emo_embedding  = torch.cat((self.mood_to_hidden(response_mood_vad), bert_hidden,  self.personality_to_hidden(personality)), 1)
+        emo_embedding  = torch.cat((self.mood_to_hidden(response_mood_vad), self.hidden_resize_2(bert_hidden),  self.personality_to_hidden(personality)), 1)
         response_emo   = self.classifier(emo_embedding)
         return response_mood_vad, response_mood_logits, response_emo
 
