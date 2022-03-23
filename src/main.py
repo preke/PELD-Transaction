4dd0ad2674324d88d9cd3738e1d777340445eb6e
preke <geek00021@gmail.com>
2022-03-23 16:12:23 +0800

    .

--- src/main.py
+++ src/main.py
@@ -26,7 +26,7 @@ args.adam_epsilon  = 1e-8
 args.epochs        = 50
 
 
-seed_list = [0, 1, 2, 41, 42, 123, 234, 345, 456, 567]
+seed_list = [41, 42, 123, 234, 345, 456, 567]
 for seed in  seed_list:
     args.SEED          = seed
     args.result_name   = 'BERT_baseline_' + str(args.SEED) + '.csv'
