79d631e1083933482dab6f7b4194ce94bc40e810
preke <geek00021@gmail.com>
2022-03-25 14:32:52 +0800

    .

--- src/train.py
+++ src/train.py
@@ -17,31 +17,59 @@ from utils import Emotion_dict
 
 
 
-class FocalLoss(nn.Module):
-    def __init__(self, gamma=2, weight=None, reduction='mean'):
-        super(FocalLoss, self).__init__()
+class MultiFocalLoss(nn.Module):
+    """
+    Focal_Loss= -1*alpha*((1-pt)**gamma)*log(pt)
+    Args:
+        num_class: number of classes
+        alpha: class balance factor shape=[num_class, ]
+        gamma: hyper-parameter
+        reduction: reduction type
+    """
+
+    def __init__(self, num_class, alpha=None, gamma=2, reduction='mean'):
+        super(MultiFocalLoss, self).__init__()
+        self.num_class = num_class
         self.gamma = gamma
-        self.weight = weight
         self.reduction = reduction
- 
-    def forward(self, output, target):
-        # convert output to pseudo probability
-        out_target = torch.stack([output[i, t] for i, t in enumerate(target)])
-        probs = torch.sigmoid(out_target)
-        focal_weight = torch.pow(1-probs, self.gamma)
- 
-        # add focal weight to cross entropy
-        ce_loss = F.cross_entropy(output, target, weight=self.weight, reduction='none')
-        focal_loss = focal_weight * ce_loss
- 
+        self.smooth = 1e-4
+        self.alpha = alpha
+        if alpha is None:
+            self.alpha = torch.ones(num_class, ) - 0.5
+        elif isinstance(alpha, (int, float)):
+            self.alpha = torch.as_tensor([alpha] * num_class)
+        elif isinstance(alpha, (list, np.ndarray)):
+            self.alpha = torch.as_tensor(alpha)
+        if self.alpha.shape[0] != num_class:
+            raise RuntimeError('the length not equal to number of class')
+
+    def forward(self, logit, target):
+        # assert isinstance(self.alpha,torch.Tensor)\
+        alpha = self.alpha.to(logit.device)
+        prob = F.softmax(logit, dim=1)
+
+        if prob.dim() > 2:
+            # used for 3d-conv:  N,C,d1,d2 -> N,C,m (m=d1*d2*...)
+            N, C = logit.shape[:2]
+            prob = prob.view(N, C, -1)
+            prob = prob.transpose(1, 2).contiguous()  # [N,C,d1*d2..] -> [N,d1*d2..,C]
+            prob = prob.view(-1, prob.size(-1))  # [N,d1*d2..,C]-> [N*d1*d2..,C]
+
+        ori_shp = target.shape
+        target = target.view(-1, 1)
+
+        prob = prob.gather(1, target).view(-1) + self.smooth  # avoid nan
+        logpt = torch.log(prob)
+        # alpha_class = alpha.gather(0, target.squeeze(-1))
+        alpha_weight = alpha[target.squeeze().long()]
+        loss = -alpha_weight * torch.pow(torch.sub(1.0, prob), self.gamma) * logpt
+
         if self.reduction == 'mean':
-            focal_loss = (focal_loss/focal_weight.sum()).sum()
-        elif self.reduction == 'sum':
-            focal_loss = focal_loss.sum()
- 
-        return focal_loss
+            loss = loss.mean()
+        elif self.reduction == 'none':
+            loss = loss.view(ori_shp)
 
-
+        return loss
 
 
 def train_model(model, args, train_dataloader, valid_dataloader, test_dataloader):
@@ -92,8 +120,8 @@ def train_model(model, args, train_dataloader, valid_dataloader, test_dataloader
             response_mood_vad, response_mood_logits, response_emo = model(b_input_ids, b_attn_masks, b_uttr_vad, b_personality, b_init_mood)
             
             mood_mse_lf  = nn.MSELoss()
-            mood_cls_lf  = FocalLoss() # nn.CrossEntropyLoss()
-            emo_loss_fct = FocalLoss() # nn.CrossEntropyLoss()
+            mood_cls_lf  = MultiFocalLoss(num_class=4, gamma=2.0, reduction='mean') # nn.CrossEntropyLoss()
+            emo_loss_fct = MultiFocalLoss(num_class=7, gamma=2.0, reduction='mean') # nn.CrossEntropyLoss()
 
             emo_loss      = emo_loss_fct(response_emo, b_labels)
             mood_mse_loss = mood_mse_lf(torch.sign(response_mood_vad), b_response_mood_vad)
