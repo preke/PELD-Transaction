1f4758d8ddfe726495b0f82762cd3a0f7c3e334c
preke <geek00021@gmail.com>
2022-03-31 10:19:14 +0800

    .

--- src/model.py
+++ src/model.py
@@ -6,10 +6,6 @@ import torch
 import re
 
 
-
-
-
-
 # ======== BERT ========
 
 class Dense(nn.Module):
@@ -43,12 +39,12 @@ class Emo_Generation(BertPreTrainedModel):
 
         self.hidden_resize = Dense(config.hidden_size, config.hidden_size, self.mid_size)
         
-        self.personality_to_hidden = nn.Linear(3, self.mid_size)
+        # self.personality_to_hidden = nn.Linear(3, self.mid_size)
         # self.personality_dense = Dense(3, self.mid_size, 3)
 
         self.hidden_to_vad = Dense(config.hidden_size, config.hidden_size, 3)
 
-        self.classifier = nn.Linear(self.mid_size*3, 7)
+        self.classifier = nn.Linear(self.mid_size*2, 7)
 
     def forward(self, input_ids, attn_masks, uttr_vad, personality, init_mood):
         
@@ -57,12 +53,12 @@ class Emo_Generation(BertPreTrainedModel):
 
         delta_mood           = torch.cat((uttr_vad, self.hidden_resize(bert_hidden)), 1) 
         
-        response_mood_vad    = F.softmax(self.mood_dense(delta_mood) * personality) + init_mood
-        # response_mood_vad    = F.softmax(self.mood_dense(delta_mood)) + init_mood
+        # response_mood_vad    = F.softmax(self.mood_dense(delta_mood) * personality) + init_mood
+        response_mood_vad    = F.softmax(self.mood_dense(delta_mood)) + init_mood
         
         response_mood_logits = self.mood_to_logit(response_mood_vad)
-        emo_embedding        = torch.cat((self.mood_to_hidden(response_mood_vad), bert_hidden, self.personality_to_hidden(personality)), 1)
-        # emo_embedding        = torch.cat((self.mood_to_hidden(response_mood_vad), bert_hidden), 1)
+        # emo_embedding        = torch.cat((self.mood_to_hidden(response_mood_vad), bert_hidden, self.personality_to_hidden(personality)), 1)
+        emo_embedding        = torch.cat((self.mood_to_hidden(response_mood_vad), bert_hidden), 1)
         
         response_emo         = self.classifier(emo_embedding)
 
