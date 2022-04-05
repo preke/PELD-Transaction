ec3e48f9b5ae59c5f9e6bea45f55945b282e12f7
preke <geek00021@gmail.com>
2022-04-05 10:40:13 +0800

    .

--- src/model.py
+++ src/model.py
@@ -37,7 +37,7 @@ class Emo_Generation(BertPreTrainedModel):
         self.mood_to_hidden = Dense(3, config.hidden_size, self.mid_size)
         self.mood_to_logit = Dense(3, config.hidden_size, 4)
 
-        self.vad_weight = 0.0
+        self.vad_weight = 0.9
 
         self.hidden_resize = Dense(config.hidden_size, config.hidden_size, self.mid_size)
         
